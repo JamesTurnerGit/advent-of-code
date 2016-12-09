6d06a52a43f844606f1e0400868b9c889871a078
James Turner <mail.jt@gmail.com>
2016-12-09 11:55:14 +0000

    added location tracking to "me" class

--- 2016/day 1/problem2.rb
+++ 2016/day 1/problem2.rb
@@ -15,21 +15,30 @@ class Compass
   end
 end
 
-
 class Me
   def initialize
-    @x = 0
-    @y = 0
     @distances = {north: 0,west: 0,south: 0,east: 0}
     @visited_locations = []
   end
+  def get_x
+    @distances[:north] - @distances[:south]
+  end
+  def get_y
+    @distances[:west]  - @distances[:east]
+  end
   def distance_from_origin
-    dist_x = (@distances[:north]-@distances[:south]).abs
-    dist_y = (@distances[:west] -@distances[:east ]).abs
+    dist_x = get_x.abs
+    dist_y = get_y.abs
     dist_x + dist_y
   end
   def move facing, distance
-    @distances[facing] += distance
+    for i in (0...distance)
+      @distances[facing] += 1
+      location = [get_x,get_y]
+      return true if @visited_locations.include?(location)
+      @visited_locations << location
+    end
+    false
   end
 end
 ## record x,y and store at each step and also do include? on that array
@@ -37,13 +46,15 @@ end
 
 raw_input = " L5, R1, R4, L5, L4, R3, R1, L1, R4, R5, L1, L3, R4, L2, L4, R2, L4, L1, R3, R1, R1, L1, R1, L5, R5, R2, L5, R2, R1, L2, L4, L4, R191, R2, R5, R1, L1, L2, R5, L2, L3, R4, L1, L1, R1, R50, L1, R1, R76, R5, R4, R2, L5, L3, L5, R2, R1, L1, R2, L3, R4, R2, L1, L1, R4, L1, L1, R185, R1, L5, L4, L5, L3, R2, R3, R1, L5, R1, L3, L2, L2, R5, L1, L1, L3, R1, R4, L2, L1, L1, L3, L4, R5, L2, R3, R5, R1, L4, R5, L3, R3, R3, R1, R1, R5, R2, L2, R5, L5, L4, R4, R3, R5, R1, L3, R1, L2, L2, R3, R4, L1, R4, L1, R4, R3, L1, L4, L1, L5, L2, R2, L1, R1, L5, L3, R4, L1, R5, L5, L5, L1, L3, R1, R5, L2, L4, L5, L1, L1, L2, R5, R5, L4, R3, L2, L1, L3, L4, L5, L5, L2, R4, R3, L5, R4, R2, R1, L5"
 input_array = raw_input.split(",")
+
 c = Compass.new
 me = Me.new
+
 input_array.each do |input|
   direction = input[1]
   distance = input[2..-1].to_i
   facing = c.turn direction
-  me.move facing, distance
+  break if me.move(facing, distance) == true
 end
 
 puts me.distance_from_origin
