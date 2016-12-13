d56aad1d54af4f73743b7969861e3c8ba8a3ff54
James Turner <mail.jt@gmail.com>
2016-12-13 15:42:18 +0000

    7.2 completed

--- 2016/day-07/problem-2.rb
+++ 2016/day-07/problem-2.rb
@@ -23,42 +23,41 @@ def parse_inputFile
   output
 end
 
-def single_abba? string
-  return true if string[0] == string[3] && string [1] == string[2] && !(string[0]==string[1])
+def single_ssl? string
+  return true if string[0] == string[2] && !(string[0]==string[1])
   false
 end
 
-def abba? string
+def ssl? string
   string = string.chars
+  output = []
   while true
-    return false if string.length == 3
-    return true if single_abba? string[0..3]
+    return output if string.length == 2
+    output << string [0..2].join if single_ssl? string[0..2]
     string.delete_at 0
   end
 end
 
 lines = parse_inputFile
-
 successes = 0
 
 lines.each do |bracketed,non_bracketed|
-  success = true
-  bracketed.each do |string|
-    if abba? string
-      success = false
-      break
-    end
-  end
-  if success
-    success = false
-    non_bracketed.each do |string|
-      if abba? string
-        success = true
-        break
+  success = false
+  aba = []
+  bracketed.each {|string|aba << ssl?(string)}
+  aba.flatten!
+  if aba.length > 0
+    aba.each do |sub_aba|
+      bab = "#{sub_aba[1]}#{sub_aba[0]}#{sub_aba[1]}"
+      non_bracketed.each do |string|
+        if string.include? bab
+          success = true
+          break
+        end
       end
     end
   end
-  successes += 1 if success == true
+  successes += 1 if success
 end
 
 p successes
