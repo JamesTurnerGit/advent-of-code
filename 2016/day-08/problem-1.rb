0081af1aa27faa9d69e658c4d6ca5d08b44226f6
James Turner <mail.jt@gmail.com>
2016-12-13 18:19:30 +0000

    8-1 done draw and init screens

--- 2016/day-08/problem-1.rb
+++ 2016/day-08/problem-1.rb
@@ -1,6 +1,5 @@
-##50x6
-
 require 'strscan'
+
 def parse_inputFile
   file_location = File.expand_path(File.dirname(__FILE__))
   output = []
@@ -17,4 +16,37 @@ def parse_inputFile
   output
 end
 
-p  parse_inputFile
+def init_screen x,y
+  output = []
+  y.times do
+    x_output = []
+    x.times do
+      x_output << false
+    end
+    output << x_output
+  end
+
+  output
+end
+
+def display_output array
+  array.each do |row|
+    row.each do |pixel|
+      print pixel ? "*" : "."
+    end
+    puts
+  end
+end
+
+def draw_rect array, x, y
+end
+
+def move_y array, row , ammount
+end
+
+def move_x array, row , ammount
+end
+
+screen = init_screen(50,6)
+display_output screen
+parse_inputFile
