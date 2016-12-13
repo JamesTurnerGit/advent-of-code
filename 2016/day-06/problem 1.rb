c7aad92e34a332b6c1b97f6866720c2898c51c64
James Turner <mail.jt@gmail.com>
2016-12-13 13:50:32 +0000

    6-1

--- 2016/day-06/problem 1.rb
+++ 2016/day-06/problem 1.rb
@@ -1,6 +1,7 @@
 def parse_inputFile
   output = [[],[],[],[],[],[],[],[]]
-  File.open "input","r" do |file|
+  file_location = File.expand_path(File.dirname(__FILE__))
+  File.open (File.join(file_location, "input")),"r" do |file|
     file.each do |line|
       line.chomp.chars.each_with_index do |char,index|
         output[index] << char
@@ -14,7 +15,7 @@ def most_common chars
   output = {}
   ##count letters
   for letter in "a".."z" ##count letters
-    count = string.count(letter)
+    count = chars.count(letter)
     if count > 0
       output[count] ||= ""
       output[count] += letter
@@ -27,5 +28,9 @@ def most_common chars
   output[0]
 end
 
-input = parse_inputFile
-input.each
+characters = parse_inputFile
+characters.map! do |char|#
+ char = most_common char
+end
+
+puts characters.join
